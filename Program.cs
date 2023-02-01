3a7dc429b4cd4d0b0cfc23044585ccd54d191837
Anna <a1b2v3@mail.ru>
2023-02-01 16:46:02 +0500

    add blocksheme

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿void Fill(string[]array)
+﻿// Метод заполнения одномерного массива из строк
+
+void Fill(string[]array)
 {
 
 for(int i = 0; i < array.Length; i++)
@@ -14,15 +16,10 @@ string Print1(string[]array)
 }
 
 
-// Создание нового массива из первого по указанным условиям
-//void NewArray(string[]array)
-//{
-
   
-    
 
-
-string[]array = new string[3];
+// Подсчет длины нового массива. Она должна быть равна количеству элементов старого массива, содержащих не более 3 символов.
+string[]array = new string[7];
 Fill(array);
 Print1(array);
 
@@ -36,28 +33,15 @@ for ( int i = 0; i < array.Length; i++)
 
 }
 
-
+// Заполнение  нового массивва заданной длины
 string[]newArray = new string[count];
 for ( int i = 0; i < array.Length; i++)
 {
     if ( array[i].Length <=3)
         newArray[i]=array[i];
 
-
-        // for ( int j=0; j<count; j++) 
-        // {
-        //     newArray[j]=array[i];
-        // }       
-         // string array_text = Print1 (newArray);
-         // Console.Write(array_text);
-         // Console.Write($"[{string.Join(", ", newArray)}]");
-          //Console.Write( array[i]);
-          //Console.Write("   ");
-         //return (newArray);
-} 
-        
-     //string array_text = Print1 (newArray);
-     //Console.Write(array_text);
+}
+       
     
 string array_text = Print1 (newArray);
 Console.WriteLine(array_text);  
