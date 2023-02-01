c91168fbae04b9edb4c52525d47fa43df9702a52
AnnaVBelousova <119129773+AnnaVBelousova@users.noreply.github.com>
2023-02-01 21:54:45 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -14,13 +14,6 @@ string Print1(string[]array)
 }
 
 
-// Создание нового массива из первого по указанным условиям
-//void NewArray(string[]array)
-//{
-
-  
-    
-
 
 string[]array = new string[3];
 Fill(array);
@@ -44,20 +37,10 @@ for ( int i = 0; i < array.Length; i++)
         newArray[i]=array[i];
 
 
-        // for ( int j=0; j<count; j++) 
-        // {
-        //     newArray[j]=array[i];
-        // }       
-         // string array_text = Print1 (newArray);
-         // Console.Write(array_text);
-         // Console.Write($"[{string.Join(", ", newArray)}]");
-          //Console.Write( array[i]);
-          //Console.Write("   ");
-         //return (newArray);
+       
 } 
         
-     //string array_text = Print1 (newArray);
-     //Console.Write(array_text);
+     
     
 string array_text = Print1 (newArray);
 Console.WriteLine(array_text);  
